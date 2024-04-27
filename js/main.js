3f7874730e45bf03a5a1c1b26db4ccf21e9d68e0
ankbat <ankbat@gmail.com>
2024-04-27 20:46:33 +0530

    updated

--- js/main.js
+++ js/main.js
@@ -17,14 +17,14 @@
     AOS.init();
 
 
-    // Sticky Navbar
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 45) {
-            $('.navbar').addClass('sticky-top shadow-sm');
-        } else {
-            $('.navbar').removeClass('sticky-top shadow-sm');
-        }
-    });
+    // // Sticky Navbar
+    // $(window).scroll(function () {
+    //     if ($(this).scrollTop() > 45) {
+    //         $('.navbar').addClass('sticky-top shadow-sm');
+    //     } else {
+    //         $('.navbar').removeClass('sticky-top shadow-sm');
+    //     }
+    // });
 
 
     // Hero Header carousel
