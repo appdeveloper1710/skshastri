3174958823415630eba1b2952af541bdc9319e88
ankbat <ankbat@gmail.com>
2024-04-29 16:02:41 +0530

    - counter added

--- js/main.js
+++ js/main.js
@@ -10,6 +10,84 @@
         }, 1);
     };
     spinner(0);
+
+    const counterCircle_love = document.getElementById('counterCircle-love');
+    const counterText_love = document.getElementById('counterText-love');
+
+    const counterCircle_marriage = document.getElementById('counterCircle-marriage');
+    const counterText_marriage = document.getElementById('counterText-marriage');
+
+    const counterCircle_family = document.getElementById('counterCircle-family');
+    const counterText_family = document.getElementById('counterText-family');
+
+    var counterInterval_love; 
+    var counterInterval_marriage; 
+    var counterInterval_family; 
+
+    const love_targetValue = 1000;
+    const marriage_targetValue = 50;
+    const family_targetValue = 40;
+
+    let counter_love = 0;
+    let counter_marriage = 0;
+    let counter_family = 0;
+
+    function updateCounter_love() {
+        counter_love++;
+      counterText_love.textContent = counter_love;
+      if (counter_love >= love_targetValue) {
+        clearInterval(counterInterval_love);
+        counterText_love.textContent = love_targetValue;
+      }
+    }
+
+    function updateCounter_marriage() {
+        counter_marriage++;
+        counterText_marriage.textContent = counter_marriage + "K+";
+      if (counter_marriage >= marriage_targetValue) {
+        clearInterval(counterInterval_marriage);
+        counterText_marriage.textContent = marriage_targetValue + "K+";
+      }
+    }
+
+    function updateCounter_family() {
+        counter_family++;
+        counterText_family.textContent = counter_family + "K+";
+      if (counter_family >= family_targetValue) {
+        clearInterval(counterInterval_family);
+        counterText_family.textContent = family_targetValue + "K+";
+      }
+    }
+
+     // Function to check if element is in viewport
+     function isElementInViewport(el) {
+        const rect = el.getBoundingClientRect();
+        return (
+          rect.top >= 0 &&
+          rect.left >= 0 &&
+          rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+        );
+      }
+  
+      function runCounterWhenVisible() {
+        if (isElementInViewport(counterCircle_love)) {
+           counterCircle_love.style.opacity = 1;
+           counterInterval_love = setInterval(updateCounter_love, 10);
+           window.removeEventListener('scroll', runCounterWhenVisible);
+        }
+        if (isElementInViewport(counterCircle_marriage)) {
+            counterCircle_marriage.style.opacity = 1;
+            counterInterval_marriage = setInterval(updateCounter_marriage, 200);
+            window.removeEventListener('scroll', runCounterWhenVisible);
+         }
+         if (isElementInViewport(counterCircle_family)) {
+            counterCircle_family.style.opacity = 1;
+             counterInterval_family = setInterval(updateCounter_family, 200);
+             window.removeEventListener('scroll', runCounterWhenVisible);
+          }
+      }
+  
+      window.addEventListener('scroll', runCounterWhenVisible);
     
     
     // Initiate the wowjs
